b895ed2108d6b78f9b9113771a66233d5adc2ddf
Diego José da Silva <diegojose969@gmail.com>
2026-03-02 20:34:38 -0300

    Add files via upload

---
+++ PrjPrimeiraAulaPOO2/PrjPrimeiraAulaPOO2/src/main/java/com/mycompany/prjprimeiraaulapoo2/Calculadora.java
@@ -0,0 +1,19 @@
+package com.mycompany.prjprimeiraaulapoo2;
+
+public class Calculadora {
+    public float somar(float nro1, float nro2){
+        return nro1 + nro2;
+    }
+    
+    public float multiplicar(float nro1, float nro2){
+        return nro1 * nro2;
+    }
+    
+    public float dividir(float nro1, float nro2){
+        return nro1/nro2;
+    }
+    
+    public float subtrair(float nro1, float nro2){
+        return nro1 - nro2;
+    }
+}
---
+++ PrjPrimeiraAulaPOO2/PrjPrimeiraAulaPOO2/src/main/java/com/mycompany/prjprimeiraaulapoo2/PrjPrimeiraAulaPOO2.java
@@ -0,0 +1,9 @@
+package com.mycompany.prjprimeiraaulapoo2;
+
+
+public class PrjPrimeiraAulaPOO2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
